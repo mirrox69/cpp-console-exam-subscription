6e3cc2a40238c402af9abfdf4b8318996acbbec9
mirrox69 <mironchik.ivan@inbox.ru>
2025-05-29 14:35:05 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #8. Exam Subscription\n";
+    cout << "Author: Ivan Mironchik\n";
+    return 0;
+}
